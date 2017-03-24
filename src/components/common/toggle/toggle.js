cad7744c2b974f41a33df6b2ce7e8cf9a4068cdc
Wietse Neven <hello@wietseneven>
2017-03-24 10:25:58 -0500

    Initial commit / fork from 24ways-bits

---
+++ src/components/common/toggle/toggle.js
@@ -0,0 +1,37 @@
+import Velocity from 'velocity-animate';
+
+class cToggle {
+  constructor(element) {
+    this.element = element;
+    this.button = element.querySelector('.c-toggle__button');
+    this.content = element.querySelector('.c-toggle__content');
+
+    this.open = false;
+
+    this.watch();
+  }
+
+  watch() {
+    this.button.addEventListener('click', this.toggle.bind(this));
+  }
+
+  toggle() {
+    let direction = "slideDown";
+    this.element.classList.add('c-toggle--open');
+    if (this.open) {
+      direction = "slideUp";
+      this.element.classList.remove('c-toggle--open');
+    }
+    this.open = !this.open;
+    Velocity(this.content, direction, {
+      duration: 500
+    });
+  }
+}
+
+(function(d) {
+  const toggles = d.querySelectorAll('.c-toggle');
+  for (let toggle of toggles) {
+    new cToggle(toggle);
+  }
+})(document);
